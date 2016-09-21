ed20c560adcdc0e5c62f22066b7cb3e4666d49d3
zahraj <hzahradnik@gmail.com>
2016-09-21 22:39:55 +0200

    flash notice, creation of task

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
+	add_flash_types :success, :warning, :danger, :info
+  
   def about
   end
 end
--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -4,7 +4,34 @@ class TaskController < ApplicationController
   	@tasks = Task.where(:parrent_id=> nil)
   end
 
+  def new
+  
+  end
+
+  def edit
+
+  end
+
+  def create
+  	@task = Task.new task_params
+  	@task.status = 1
+    if @task.save
+      redirect_to redirect_path, success: 'Task was successfully created.'
+      return
+    end
+
+    render :new
+  end
+
+  def task_params
+    params.require(:task).permit :title
+  end
+
   def statistics
+  
   end
   
+  def redirect_path
+    root_path
+  end
 end
