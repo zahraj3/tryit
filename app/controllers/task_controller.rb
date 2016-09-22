af2f89b166af54b3c1c44723819d88f8d8bef595
zahraj <hzahradnik@gmail.com>
2016-09-22 09:23:28 +0200

    Task show, creation sub task

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -1,26 +1,34 @@
 class TaskController < ApplicationController
 
   def index
-  	@tasks = Task.where(:parrent_id=> nil)
+    @tasks = Task.where(:parrent_id=> nil)
+  end
+
+  def show
+    @task = Task.find params[:id]
+    @tasks = Task.where(:parrent_id=> params[:id])
+    render :index
   end
 
   def new
   
   end
 
-  def edit
-
-  end
-
   def create
   	@task = Task.new task_params
   	@task.status = 1
+    @task.parrent_id = params[:task][:parrent_id]
     if @task.save
-      redirect_to redirect_path, success: 'Task was successfully created.'
-      return
+      #redirect_to action: show, id: 3
+     redirect_to action: "show", id: params[:task][:parrent_id]
+      #redirect_to redirect_path, success: 'Task was successfully created.'
+      #return
     end
+    
+  end
 
-    render :new
+  def edit
+    @task = Task.find params[:id]
   end
 
   def task_params
