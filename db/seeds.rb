aa94055cb2b1800342045ce437ba9e0e386e3712
zahraj <hzahradnik@gmail.com>
2016-09-25 21:55:23 +0200

    Seed; Update view - edit, show, index

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,33 @@
 Task.create title: 'Root Task', status: 1, parrent_id: 0
+Task.create title: 'Prvni task list - analyza', status: 1, parrent_id: 1
+Task.create title: 'Druhy task list - nakupy', status: 1, parrent_id: 1
+
+Task.create title: 'Obrazovky', status: 1, parrent_id: 2
+Task.create title: 'Rozhrani', status: 0, parrent_id: 2
+Task.create title: 'Dokumenty', status: 1, parrent_id: 2
+Task.create title: 'Reporty', status: 1, parrent_id: 2
+
+Task.create title: 'Obraz 1', status: 0, parrent_id: 4
+Task.create title: 'Obraz 2', status: 1, parrent_id: 4
+Task.create title: 'Obraz 3', status: 2, parrent_id: 4
+
+Task.create title: 'Tisk', status: 0, parrent_id: 5
+Task.create title: 'Prsn', status: 0, parrent_id: 5
+Task.create title: 'WebService', status: 0, parrent_id: 5
+
+Task.create title: 'Dok1', status: 0, parrent_id: 6
+Task.create title: 'Dok2', status: 0, parrent_id: 6
+Task.create title: 'Dok3', status: 2, parrent_id: 6
+Task.create title: 'Dok4', status: 2, parrent_id: 6
+Task.create title: 'Dok5', status: 2, parrent_id: 6
+Task.create title: 'Dok6', status: 2, parrent_id: 6
+
+Task.create title: 'Report1', status: 1, parrent_id: 7
+Task.create title: 'Report2', status: 2, parrent_id: 7
+
+Task.create title: 'Dojit nakoupit', status: 0, parrent_id: 3
+Task.create title: 'Zaplatit fakturu 123', status: 1, parrent_id: 3
+Task.create title: 'Zavolat H.', status: 2, parrent_id: 3
+Task.create title: 'Další úkol', status: 2, parrent_id: 3
+
+
